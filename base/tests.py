1bd7d263f9a36def83c1e23812f0d8b0a97ea64a
Karol Bautrel <karol.bautrel@gmail.com>
2022-02-16 23:09:10 +0100

    Addes few basics authorization tests

--- base/tests.py
+++ base/tests.py
@@ -14,7 +14,6 @@ class AuthorizationTestCase(TestCase):
             password = 'test21',
             name = 'tes ffasf',
         )
-
         self.client.force_login(self.user)
 
 
@@ -26,7 +25,7 @@ class AuthorizationTestCase(TestCase):
         self.assertEqual(response.status_code,200)
         
 
-    def test_unauthorized_user_redirect_to_login_page_after_contact_page(self):    
+    def test_unauthorized_is_able_to_contact_page(self):    
         '''
         Unauthorized user is redirect to login page after clicking contact button
         '''
@@ -34,28 +33,28 @@ class AuthorizationTestCase(TestCase):
         url = reverse('contact')
         response = self.user.get(url)
 
-        self.assertRedirects(response,'/accounts/login/?next=/contact', 302)        
+        self.assertEqual(response.status_code, 200)    
 
 
-    def test_authorized_user_to_is_able_to_add_to_cart(self):
+    def test_authorized_user_is_able_to_add_to_cart(self):
         '''
         Authorized user is able to add product to cart
         '''
-        product = Product.objects.create(name='test1234',  price=200)
-        url = reverse('buy-product', args=str((product.id)))
+        product = Product.objects.create(title='test1234',  price=200, slug='test1234')
+        url = reverse('add-to-cart', kwargs={'slug': product.slug})
         response = self.client.get(url)
         response_button = self.client.post(url, {'submit':'Confirm'})
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response_button.status_code, 200)
+        self.assertRedirects(response, '/order_summary/', status_code = 302)
+        self.assertEqual(response_button.status_code, 302)
     
 
-    def test_redirect_unathorized_user_to_login_after_buy_action(self):
+    def test_redirect_unathorized_user_to_login_after_add_to_cart_action(self):
         '''
-        Unauthorized user is unable to buy a product
+        Unauthorized user is unable to add to cart product
         '''
         self.user = Client()
-        product = Product.objects.create(name='test1234', price=200)
-        url = reverse('buy-product', args=str((product.id)))
+        product = Product.objects.create(title='test1234',  price=200, slug='test1234')
+        url = reverse('add-to-cart', kwargs={'slug': product.slug})
         response = self.user.get(url)
         
         self.assertEqual(response.status_code, 302)
@@ -82,4 +81,32 @@ class AuthorizationTestCase(TestCase):
 
         self.assertEqual(response.status_code, 200)
 
-   
+
+    def test_regular_user_is_unable_to_use_mass_message(self):
+        '''
+        Superuser is able to send messages to users
+        '''
+
+        url = reverse('message')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 403)
+
+
+    def test_uanuthorized_user_has_acces_to_products_page(self):
+
+        self.client = Client()
+        url = reverse('products')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['products']),0)
+
+    def test_product_list_view(self):
+
+        Product.objects.create(title='test1234',  price=200, slug='test1234')
+        self.client = Client()
+        url = reverse('products')
+        response = self.client.get(url)
+
+        self.assertEqual(len(response.context['products']),1)
