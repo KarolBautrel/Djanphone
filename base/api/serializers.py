1db2f895caf54c6aa5dd59f4e689fb0531646e90
Karol Bautrel <karol.bautrel@gmail.com>
2022-02-26 08:56:24 +0100

    added django-imagekit

--- base/api/serializers.py
+++ base/api/serializers.py
@@ -11,9 +11,9 @@ class ProductSerializer(ModelSerializer):
 class StoreSerializer(ModelSerializer):
 
     products = ProductSerializer(many=True)
-
+    
     class Meta:
-
+        
         model = Store
         fields = ['city','street','owner','contact','products']
         
